44ad8887923bece7603e33084e4de3dda2a0299f
caahg <caroline.carollgonzaga@gmail.com>
2025-09-23 11:20:14 +0200

    Atualizando arquivo data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+# Arquivo com constantes
+URBAN_ROUTES_URL ='https://cnt-6c272107-77c0-4208-9822-9a867b7ca4d5.containerhub.tripleten-services.com/'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO= '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
